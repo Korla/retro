a86e945219b9069aa2ffb93853d6e65a07769bc2
korla <johannes.bengtsson@raysearchlabs.com>
2016-11-19 22:16:54 +0100

    Material design

--- public/index.js
+++ public/index.js
@@ -1,10 +1,61 @@
 var config = {
-  apiKey: "apiKey",
-  authDomain: "projectId.firebaseapp.com",
-  databaseURL: "https://databaseName.firebaseio.com",
-  storageBucket: "bucket.appspot.com"
+  apiKey: "AIzaSyD03ggT9NXR-iY2o2WXmdu0GEmtTMJu8WY",
+  authDomain: "retro-976fa.firebaseapp.com",
+  databaseURL: "https://retro-976fa.firebaseio.com",
 };
 firebase.initializeApp(config);
+let dbRef = firebase.database().ref('db');
 
-// Get a reference to the database service
-var database = firebase.database();
+Vue.use(VueMdl.default)
+var app = new Vue({
+  components: VueMdl.components,
+  directives: VueMdl.directives,
+  el: '#app',
+  data: {
+    initialized: false,
+    opinions: [],
+    newOpinion: {
+      text: '',
+      value: true,
+      votes: 0
+    },
+    db: []
+  },
+  ready: function() {
+    this.$els.newOpinionElement.focus();
+  },
+  methods: {
+    add: function() {
+      if(!this.newOpinion.text) return;
+      this.opinions.push(this.newOpinion);
+      this.clear();
+      this.$els.newOpinionElement.focus();
+    },
+    clear: function() {
+      this.newOpinion = {
+        text: '',
+        value: true,
+        votes: 0
+      };
+    },
+    publish: function(opinion) {
+      this.db.push(this.remove(opinion));
+      dbRef.set(this.db);
+    },
+    remove: function(opinion) {
+      return this.opinions.splice(this.opinions.indexOf(opinion), 1)[0];
+    },
+    vote: function(opinion) {
+      opinion.votes++;
+      dbRef.set(this.db);
+    },
+    reset: function() {
+      dbRef.set([]);
+    },
+  }
+});
+
+dbRef.on('value', data => {
+  app.initialized = true;
+  app.db = data.val() || [];
+});
